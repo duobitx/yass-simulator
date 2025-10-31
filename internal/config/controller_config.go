6b638b8852382ef859c3c6d43e7fff4163cd9947
Marek Gruszecki <marek.gruszecki@duobit.pl>
2025-10-31 19:15:04 +0100

    change docker image

--- internal/config/controller_config.go
+++ internal/config/controller_config.go
@@ -7,13 +7,15 @@ import (
 	v1 "k8s.io/api/core/v1"
 )
 
+const InternalComponentsImage = "ghcr.io/duobitx/yass/internal-components:latest"
+
 type Configuration struct {
 	InternalComponentImage           string
 	InternalComponentImagePullPolicy v1.PullPolicy
 }
 
 func NewConfiguration() (*Configuration, error) {
-	image := goutils.Env("INTERNAL_COMPONENTS_IMAGE", "ghcr.io/esa-philab/yass/internal-components:latest")
+	image := goutils.Env("INTERNAL_COMPONENTS_IMAGE", InternalComponentsImage)
 	imagePullPolicy := v1.PullIfNotPresent
 	if !strings.Contains(image, ":") || strings.HasSuffix(image, "latest") {
 		imagePullPolicy = v1.PullAlways
