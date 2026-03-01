5735ae04ec8269ff5932c429bc149a73a3c5cf0f
Marek Gruszecki <marek.gruszecki@duobit.pl>
2026-03-01 23:31:50 +0100

    fix: tests

--- experiment-executor/internal/experiment_spec_test.go
+++ experiment-executor/internal/experiment_spec_test.go
@@ -10,11 +10,11 @@ import (
 
 func TestLoad(t *testing.T) {
 	absExperimentPath, _ := os.Getwd()
-	absExperimentPath += "/../experiment.json"
+	absExperimentPath += "/experiment.json"
 	absExperimentPath, err := filepath.Abs(absExperimentPath)
 	assert.NoError(t, err)
 
-	t.Setenv("EXPERIMENT_FILE", "../experiment.json")
+	t.Setenv("EXPERIMENT_FILE", "./experiment.json")
 	t.Setenv("EXPERIMENT_JSON_FILE_PATH", absExperimentPath)
 	_, err = LoadExperimentJson()
 	assert.NoError(t, err)
