755e7b76da6459b91f815e73498587e9f14cc040
Marek Gruszecki <marek.gruszecki@duobit.pl>
2026-03-01 23:37:18 +0100

    fix: tests

--- experiment-executor/internal/experiment_spec_test.go
+++ experiment-executor/internal/experiment_spec_test.go
@@ -1,21 +1,13 @@
 package internal
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLoad(t *testing.T) {
-	absExperimentPath, _ := os.Getwd()
-	absExperimentPath += "/experiment.json"
-	absExperimentPath, err := filepath.Abs(absExperimentPath)
-	assert.NoError(t, err)
-
-	t.Setenv("EXPERIMENT_FILE", "./experiment.json")
-	t.Setenv("EXPERIMENT_JSON_FILE_PATH", absExperimentPath)
-	_, err = LoadExperimentJson()
+	t.Setenv("EXPERIMENT_FILE", "../experiment.json")
+	_, err := LoadExperimentJson()
 	assert.NoError(t, err)
 }
